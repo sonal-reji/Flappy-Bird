8927ce9452f49bbefba107afdfb2f44576969b37
Sonal Reji <sonalreji@gmail.com>
2025-06-21 09:39:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ gameOverImg.src = "./Images/flappy-gameover.png";
 let bird = {
     x: 50,
     y: boardHeight / 2,
-    width: 100,
-    height: 100
+    width: 80,
+    height: 80
 }
 
 let velocityY = 0;
